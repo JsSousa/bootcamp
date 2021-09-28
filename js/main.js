85ae8aca38ac7c8821a514e9c8f534dfaf0cac44
geskaran <geskaran@gmail.com>
2021-09-27 19:43:40 -0500

    calculadora

---
+++ js/main.js
@@ -0,0 +1,79 @@
+let valorScreen = 0;
+let numeroA;
+let numeroB;
+let operation;
+
+function button(hambg) {
+    let auxiliar = document.getElementById("screen").value;
+    document.getElementById("screen").value = auxiliar + hambg;
+
+
+    valorScreen = document.getElementById("screen").value = auxiliar + hambg;
+}
+
+
+function btn_add(caracter) {
+    numeroA = valorScreen;
+    operation = "+";
+
+    clear();
+}
+
+function btn_subtract(caracter) {
+    numeroA = valorScreen;
+    operation = "-";
+
+    clear();
+}
+
+function btn_multiply(caracter) {
+    numeroA = valorScreen;
+    operation = "*";
+
+    clear();
+}
+
+function btn_divide(caracter) {
+    numeroA = valorScreen;
+    operation = "/";
+
+    clear();
+}
+
+function reset() {
+    document.getElementById('screen').value = '';
+    valorScreen = 0;
+    operation = "";
+}
+
+function clear() {
+    document.getElementById('screen').value = '';
+
+}
+
+function btn_result() {
+    numeroB = valorScreen;
+    calcular();
+}
+
+function calcular() {
+
+    let total = null;
+    switch (operation) {
+        case "+":
+            total = parseFloat(numeroA) + parseFloat(numeroB);
+            break;
+        case "-":
+            total = parseFloat(numeroA) - parseFloat(numeroB);
+            break;
+        case "*":
+            total = parseFloat(numeroA) * parseFloat(numeroB);
+            break;
+        case "/":
+            total = parseFloat(numeroA) / parseFloat(numeroB);
+            break;
+    }
+    reset();
+    document.getElementById('screen').value = total;
+    valorScreen = total;
+}
